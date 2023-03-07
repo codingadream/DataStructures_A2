9cc220e541fbdf58f110b6f7bd3e5818b4beb48c
codingadream <gladdensinfo@gmail.com>
2023-03-07 10:34:13 -0600

    Initial Set up

---
+++ Header1.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+#include <cmath>
+#include <stdexcept>
+
+
+using namespace std;
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+#include <array>
+#include <string>
+#include <cmath>
+#include <stdexcept>
+#include "Header.h"
+
+using namespace std;
